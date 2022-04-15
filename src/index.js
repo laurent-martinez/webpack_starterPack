563451ee6983d47073e8766316629bd4b244b517
laurent-martinez <martilaurier@gmail.com>
2022-04-15 17:14:52 +0200

    searchbar

--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
-import "bootstrap";
 import "./styles/main.scss";
 import { recipes } from "./recipes.js";
-// or get all of the named exports for further usage
-import * as bootstrap from "bootstrap";
-
 import cooking from "./assets/cooking_hat.svg";
 import magnifying from "./assets/magnifying_glass.svg";
 import food from "./assets/food.svg";
 import time from "./assets/timer.svg";
-console.log(recipes);
-const logoBrand = document.getElementById("logo");
-logoBrand.src = cooking;
-const searchPicture = document.getElementById("search_picture");
-searchPicture.src = magnifying;
-const recipesPic = document.querySelectorAll(".card-img-top");
-recipesPic.forEach((recip) => {
-  recip.src = food;
-});
-const logoTimer = document.querySelectorAll(".timer");
-logoTimer.forEach((logo) => {
-  logo.src = time;
-});
+
+let recipeList = [];
 
 const recipeCardTemplate = document.querySelector("[data-recipe-template]");
 const recipeCardsContainer = document.querySelector("[data-cards-container]");
 
-recipes.forEach((recipe) => {
-  const card = recipeCardTemplate.content.cloneNode(true);
+const logoBrand = document.getElementById("logo");
+logoBrand.src = cooking;
+const searchPicture = document.getElementById("search_picture");
+searchPicture.src = magnifying;
+const searchBar = document.querySelector("[data-search]");
+
+recipeList = recipes.map((recipe) => {
+  const card = recipeCardTemplate.content.cloneNode(true).children[0];
   const title = card.querySelector("[data-title]");
   const timing = card.querySelector("[data-timing]");
   const ingredients = card.querySelector("[data-ingredients]");
   const instructions = card.querySelector("[data-instructions]");
   title.textContent = recipe.name;
   timing.textContent = recipe.time;
-  console.log(recipe.ingredients);
   const ingredientso = recipe.ingredients;
   ingredientso.forEach((ing) => {
     ingredients.textContent += `${ing.ingredient} : ${
@@ -42,4 +32,33 @@ recipes.forEach((recipe) => {
   });
   instructions.textContent = recipe.description;
   recipeCardsContainer.append(card);
+  return {
+    titre: recipe.name,
+    ingred: recipe.ingredients,
+    element: card,
+  };
+});
+console.log(recipeList);
+searchBar.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  console.log(value);
+  recipeList.forEach((list) => {
+    list.ingred.forEach((ing) => {
+      const isVisible =
+        list.titre.toLowerCase().includes(value) ||
+        ing.ingredient.toLowerCase().includes(value);
+      list.element.classList.toggle("hide", !isVisible);
+    });
+  });
+});
+
+const recipesPic = document.querySelectorAll("[data-img]");
+recipesPic.forEach((recip) => {
+  recip.src = food;
+});
+
+const logoTimer = document.querySelectorAll("[data-timer-img]");
+console.log(logoTimer);
+logoTimer.forEach((logo) => {
+  logo.src = time;
 });
