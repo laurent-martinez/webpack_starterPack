7fe598208d85eba93968277fa1dfbf4a317e2c5a
laurent-martinez <martilaurier@gmail.com>
2022-04-15 10:49:19 +0200

    dynamic cards

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import "./styles/main.scss";
+import "bootstrap";
+
+// or get all of the named exports for further usage
+import * as bootstrap from "bootstrap";
+
 import cooking from "./assets/cooking_hat.svg";
+import magnifying from "./assets/magnifying_glass.svg";
+import recipes from "./assets/food.svg";
+import time from "./assets/timer.svg";
 
 const logoBrand = document.getElementById("logo");
 logoBrand.src = cooking;
+const searchPicture = document.getElementById("search_picture");
+searchPicture.src = magnifying;
+const recipesPic = document.querySelectorAll(".card-img-top");
+recipesPic.forEach((recipe) => {
+  recipe.src = recipes;
+});
+const logoTimer = document.querySelectorAll(".timer");
+logoTimer.forEach((logo) => {
+  logo.src = time;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,35 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.(scss)$/,
+        use: [
+          {
+            // inject CSS to page
+            loader: "style-loader",
+          },
+          {
+            // translates CSS into CommonJS modules
+            loader: "css-loader",
+          },
+          {
+            // Run postcss actions
+            loader: "postcss-loader",
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [require("autoprefixer")];
+                },
+              },
+            },
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+          },
+        ],
       },
       {
         test: /\.js$/,
